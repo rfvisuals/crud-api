e2535dc4c35121be22c32c4bb608425927e3505c
hiaximize <dbarkerit@gmail.com>
2019-08-13 21:22:08 -0400

    api call working from search box on index.html

--- public/js/app.js
+++ public/js/app.js
@@ -3,19 +3,19 @@ console.log("you connected?");
 const app = angular.module('MoviesApp', []);
 
 app.controller('MoviesController', ["$http", function($http) {
-    this.title = title;
+    
     this.getMovies = () => {
+        
         $http({
             method: "GET",
-            query: this.title,
-            end: "&page=1&include_adult=false",
-            url: "https://api.themoviedb.org/3/search/movie?api_key=061fedfef8ce353a68d5e4c131350183&language=en-US&query=" + query + end
+            url: "https://api.themoviedb.org/3/search/movie?api_key=061fedfef8ce353a68d5e4c131350183&language=en-US&query=" + this.title + "&page=1&include_adult=false"
+
         }).then((response)=>{
             console.log(response.data);
             console.log(response.data.overview);
             this.movies = response.data;
         });  
     };
-    this.getMovies();
+    
 
 }]); // app.controller closing
