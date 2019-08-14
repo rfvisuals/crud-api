21f779677866b21f1b0836ebc8a6e4617fb68c8a
worldpeer <contact@worldpeer.com>
2019-08-13 18:56:40 -0700

    modified models file for new API

--- models/moviesModel.js
+++ models/moviesModel.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const moviesSchema = new mongoose.Schema({
-  original_title: String,
-  vote_average: Number,
-  overview: String,
-  budget:Number
+  Title: String,
+  Year: Number,
+  Poster: String
 })
 
 const Movies = mongoose.model('Movies', moviesSchema);
--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,9 @@ const app = angular.module('MoviesApp', []);
 app.controller('MoviesController', ["$http",
 function($http) {
     this.getMovies = () => {
-
         $http({
             method: "GET",
             url: "https://api.themoviedb.org/3/search/movie?api_key=061fedfef8ce353a68d5e4c131350183&language=en-US&query=" + this.title + "&page=1&include_adult=false"
-
         }).then((response)=>{
             console.log(response.data);
             console.log(response.data.overview);
